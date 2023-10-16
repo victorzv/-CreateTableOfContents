e79bb5b13a3ec0854cde2d4d723295267086e45d
victorzv <kaplantigra@gmail.com>
2023-10-16 12:44:56 +0300

    Add BuildTestData class for test data

---
+++ TryPDFFile/BuildTestData.cs
@@ -0,0 +1,13 @@
+namespace TryPDFFile;
+
+public class BuildTestData : IBuildData
+{
+    public List<ItemInfo> parseData(string pdfDocPath)
+    {
+        List<ItemInfo> list = new List<ItemInfo>();
+
+        
+        
+        return list;
+    }
+}
