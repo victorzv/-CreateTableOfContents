75cc9e542de2fe1c93fbfaff3d2ccc58ff2fa264
victorzv <kaplantigra@gmail.com>
2023-10-16 10:48:31 +0300

    remove code from main

--- TryPDFFile/Program.cs
+++ TryPDFFile/Program.cs
@@ -8,54 +8,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        //new License().SetLicense("./test.lic");
-        Document pdfDoc = new Document("./test.pdf");
 
         List<ItemInfo> itemsList = new List<ItemInfo>();
-        ParagraphAbsorber absorb = new ParagraphAbsorber();
-        absorb.Visit(pdfDoc);
-        int counter = 1;
-        foreach (PageMarkup markup in absorb.PageMarkups)
-        {
-            foreach (MarkupSection section in markup.Sections)
-            {
-                foreach (MarkupParagraph paragraph in section.Paragraphs)
-                {
-                    foreach (List<TextFragment> line in paragraph.Lines)
-                    {
-                        float maxFont = 0;
-                        int pageNumber = 0;
-                        foreach (TextFragment textFragment in line)
-                        {
-                            if(textFragment.TextState.FontSize > maxFont)
-                            {
-                                maxFont = textFragment.TextState.FontSize;                                
-                            }
-
-                            if(textFragment.Page.Number > pageNumber)
-                            {
-                                pageNumber = textFragment.Page.Number;                                
-                            }
-
-                        }
-
-                        //Console.WriteLine(pageNumber);
-                        Console.WriteLine(paragraph.Text);
-                        Console.WriteLine(maxFont);
-                        Console.WriteLine("_____________________________");
-                        ItemInfo itemInfo = new ItemInfo()
-                        {
-                            FontSize = maxFont,
-                            Page = pageNumber,
-                            Text = paragraph.Text,
-                            AbsolutePosition = counter++
-                        };
-
-                        itemsList.Add(itemInfo);
-                    }
-                }
-            }
-        }
 
         IBuilderContent normalContent = new GenNormalContent();
 
