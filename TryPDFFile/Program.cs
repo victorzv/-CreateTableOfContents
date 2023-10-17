5922eea8a5da6c9c9fc1fd14a0ad1483622df0bd
victorzv <kaplantigra@gmail.com>
2023-10-17 18:28:48 +0300

    add read/write json

--- TryPDFFile/Program.cs
+++ TryPDFFile/Program.cs
@@ -1,17 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using Aspose.Pdf;
 using Aspose.Pdf.Text;
+using Newtonsoft.Json;
 using TryPDFFile;
 
 class Program
 {
     static void Main(string[] args)
     {
-
+        List<ItemInfo> itemsList = null;
+        
         IBuildData buildData = new BuilderPDFSimple();
+        
+        Console.WriteLine("Enter 1 to create json file from pdf file");
+        Console.WriteLine("Enter 2 to read from json file and run program");
+        Console.WriteLine("Enter 3 to read from pdf file and run program");
+        Console.WriteLine("Enter 4 to read from preset pdf file and run program");
 
-        List<ItemInfo> itemsList = buildData.parseData("./test.pdf");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int choice))
+        {
+            if (choice == 1)
+            {
+                Console.WriteLine("Enter pdf file full path:");
+                string pdfFile = Console.ReadLine();
+                if (!File.Exists(pdfFile))
+                {
+                    Console.WriteLine("File doesn't exist");
+                    return;
+                }
+                else
+                {
+                    itemsList = buildData.parseData(pdfFile);
+                    Console.WriteLine("Enter json file full path to save:");
+                    string jsonFile = Console.ReadLine();
+                    string json = JsonConvert.SerializeObject(itemsList, Formatting.Indented);
+                    File.WriteAllText(jsonFile, json);
+                    Console.WriteLine($"File {jsonFile} saved.");
+                    return;
+                }
+            }
+
+            if (choice == 2)
+            {
+                Console.WriteLine("Enter json file name full path:");
+                string jsonFile = Console.ReadLine();
+                if (!File.Exists(jsonFile))
+                {
+                    Console.WriteLine("File doesn't exist");
+                    return;
+                }
+                else
+                {
+                    string json = File.ReadAllText(jsonFile);
+                    itemsList = JsonConvert.DeserializeObject<List<ItemInfo>>(json);
+                }
+            }
+
+            if (choice == 3)
+            {
+                Console.WriteLine("Enter pdf file full path:");
+                string pdfFile = Console.ReadLine();
+                itemsList = buildData.parseData(pdfFile);        
+            }
+
+            if (choice == 4)
+            {
+                itemsList = buildData.parseData("./test.pdf");       
+            }
+        }
 
         IBuilderContent normalContent = new GenNormalContent();
 
