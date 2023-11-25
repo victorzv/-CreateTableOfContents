c9bffc37a31563e04be8efe7f9eff5afe875dc07
Evgeny Pirogov <Evgeny.Pirogov@aspose.com>
2023-11-25 22:11:38 +0200

    no choice

--- Program.cs
+++ Program.cs
@@ -8,44 +8,40 @@ class Program
     static void Main(string[] args)
     {
         new License().SetLicense(@"./test.lic");
-        List<IParagraphInfo> itemsList = null;
-        
+        List<IParagraphInfo> itemsList;
+
         IPdfParser pdfParser = new PdfParser();
 
-        String pdfFileOutline = "";
-        string input = Console.ReadLine();
+        string pdfFileOutline;
 
-        if (int.TryParse(input, out int choice))
+        Console.WriteLine("Enter pdf file full path:");
+        string pdfFile = args.Length == 0 ? Console.ReadLine() : args[0];
+        string jsonFile = Path.ChangeExtension(pdfFile, "json");
+
+        if (!File.Exists(pdfFile))
         {
-            Console.WriteLine("Enter pdf file full path:");
-            string pdfFile = args.Length == 0 ? Console.ReadLine() : args[0];
-            string jsonFile = Path.ChangeExtension(pdfFile, "json");
-            
-            if (!File.Exists(pdfFile))
-            {
-                Console.WriteLine("File doesn't exist");
-                return;
-            }
-            
-            itemsList = pdfParser.parseData(pdfFile);
-            string json = JsonConvert.SerializeObject(itemsList, Formatting.Indented);
-            File.WriteAllText(jsonFile, json);
-            Console.WriteLine($"File {jsonFile} saved.");
-
-            if (!File.Exists(jsonFile))
-            {
-                Console.WriteLine("File doesn't exist");
-                return;
-            }
-            
-            pdfFileOutline = pdfFile;
-            itemsList = pdfParser.parseData(pdfFile);
+            Console.WriteLine("File doesn't exist");
+            return;
         }
+
+        itemsList = pdfParser.parseData(pdfFile);
+        string json = JsonConvert.SerializeObject(itemsList, Formatting.Indented);
+        File.WriteAllText(jsonFile, json);
+        Console.WriteLine($"File {jsonFile} saved.");
+
+        if (!File.Exists(jsonFile))
+        {
+            Console.WriteLine("File doesn't exist");
+            return;
+        }
+
+        pdfFileOutline = pdfFile;
+        itemsList = pdfParser.parseData(pdfFile);
 
         List<IParagraphInfo> levelList = TableContentHierarchy.SetupLevel(itemsList);
 
         Document pdfDoc = new Document(pdfFileOutline);
-            
+
         foreach (var item in levelList)
         {
             OutlineItemCollection outlineItemCollection = new OutlineItemCollection(pdfDoc.Outlines);
@@ -53,18 +49,17 @@ class Program
             outlineItemCollection.Italic = true;
             outlineItemCollection.Action =
                 new GoToAction(new XYZExplicitDestination(pdfDoc, item.Page, item.Left, item.Top, 1));
-                
+
             pdfDoc.Outlines.Add(outlineItemCollection);
 
         }
 
         pdfDoc.Save(pdfFileOutline);
-        foreach(var item in levelList)
+        foreach (var item in levelList)
         {
             Console.WriteLine($"{new string(' ', item.Level * 4)} FS({item.FontSize}) Page ({item.Page}) Text: {item.Text} AP {item.AbsolutePosition}");
         }
-        
-        
+
         /*
         IBuilderContent normalContent = new GenNormalContent();
 
